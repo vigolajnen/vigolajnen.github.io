29f5e88e2c0321c098bd31ded4e28a7913550c26
Andrey Shaurov <andrey.shaurov@gmail.com>
2019-02-18 16:37:30 +0300

    конкурс

---
+++ konkurs/index.php
@@ -0,0 +1,311 @@
+<?
+require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
+?>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="utf-8">
+  <title>Конкурс проектов от «Евромобайл»</title>
+  <meta http-equiv="x-ua-compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="description" content="">
+  <meta property="og:locale" content="ru_RU">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="">
+  <meta property="og:description" content="">
+  <meta property="og:url" content="">
+  <meta property="og:site_name" content="Nesco">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:description" content="">
+  <meta name="twitter:title" content="">
+  <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+  <link rel="stylesheet" href="css/style.min.css">
+</head>
+
+<body>
+  <!-- Yandex.Metrika counter -->
+  <script type="text/javascript">
+      (function (d, w, c) {
+          (w[c] = w[c] || []).push(function () {
+              try {
+                  w.yaCounter26116248 = new Ya.Metrika({
+                      id: 26116248,
+                      clickmap: true,
+                      trackLinks: true,
+                      accurateTrackBounce: true,
+                      webvisor: true
+                  });
+              } catch (e) {
+              }
+          });
+
+          var n = d.getElementsByTagName("script")[0],
+                  s = d.createElement("script"),
+                  f = function () {
+                      n.parentNode.insertBefore(s, n);
+                  };
+          s.type = "text/javascript";
+          s.async = true;
+          s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+          if (w.opera == "[object Opera]") {
+              d.addEventListener("DOMContentLoaded", f, false);
+          } else {
+              f();
+          }
+      })(document, window, "yandex_metrika_callbacks");
+  </script>
+  <noscript>
+  <div><img src="https://mc.yandex.ru/watch/26116248" style="position:absolute; left:-9999px;" alt=""/></div>
+  </noscript>
+  <!-- /Yandex.Metrika counter -->
+
+  <!-- Google Analytics counter start -->
+  <script>
+      (function (i, s, o, g, r, a, m) {
+          i['GoogleAnalyticsObject'] = r;
+          i[r] = i[r] || function () {
+              (i[r].q = i[r].q || []).push(arguments)
+          }, i[r].l = 1 * new Date();
+          a = s.createElement(o),
+                  m = s.getElementsByTagName(o)[0];
+          a.async = 1;
+          a.src = g;
+          m.parentNode.insertBefore(a, m)
+      })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+      ga('create', 'UA-54599179-1', 'auto');
+      ga('send', 'pageview');
+
+  </script>
+  <!-- Google Analytics counter end -->
+  <div class="wrapper">
+    <header id="header-fixed" class="page-header">
+      <div class="page-header__box">
+        <div class="page-header__logo">
+          <a class="logo" href="https://iot.ru" target="_blank">
+            <svg class="logo__pic" width="18" height="18">
+              <use xlink:href="img/sprite.svg#icon-iot-logo"></use>
+            </svg>
+          </a>
+        </div>
+        <nav class="page-header__nav nav navigation navigation--closed navigation--nojs">
+          <button class="navigation__toggle js-mobile-menu-open" type="button" aria-label="Открыть меню">
+          </button>
+          <div class="navigation__wrapper">
+            <ul class="navigation__list">
+              <li class="navigation__item">
+                <a class="navigation__link js-promo" href="#">о конкурсе</a>
+              </li>
+              <li class="navigation__item">
+                <a class="navigation__link js-organizers" href="#organizers">организаторы</a>
+              </li>
+              <li class="navigation__item">
+                <a class="navigation__link js-advantages" href="#advantages">зачем участвовать</a>
+              </li>
+              <li class="navigation__item">
+                <a class="navigation__link js-conditions" href="#conditions">условия участия</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <section id="promo" class="promo">
+      <canvas id="nokey">
+        Your Browser Don't Support Canvas, Please Download Chrome ^_^``
+      </canvas>
+      <div class="promo__info">
+        <h1 class="promo__title">Инжиниринговая компания «ЕвроМобайл» поможет реализовать ваши самые амбициозные
+          проекты</h1>
+        <p class="promo__text">Российская инжиниринговая компания «ЕвроМобайл» ищет проекты для реализации в решениях
+          Smart City</p>
+        <button class="button button-order popup-btn">Отправить заявку</button>
+      </div>
+    </section>
+    <section id="organizers" class="organizers">
+      <div class="box">
+        <div class="box__item box__item--info">
+          <div class="box__info">
+            <h2 class="title box__title">Организаторы</h2>
+            <div class="box__caption">Эксперт конкурса:</div>
+            <a href="https://www.euromobile.ru/" target="_blank">
+              <img src="img/euromob-logo-dark.svg" alt="логотип компании">
+            </a>
+            <h4 class="box__title-h4">Российская инжиниринговая компания «ЕвроМобайл»</h4>
+            <p>Компания «ЕвроМобайл» уже 15 лет находится на гребне инновационной волны и реализует бизнес-проекты,
+              которые работают по всей России. Если вы хотите встать в один ряд с профессионалом рынка интернета вещей
+              и
+              дать жизнь своему изобретению, то вы – на правильном пути!</p>
+            <p>Нашими проектами пользуются: РЖД, Сбербанк, МТС, Горэлектротранс, Штадлер и другие крупнейшие
+              государственные и частные компании из таких областей как: машиностроение транспорт, банковская сфера,
+              ЖКХ,
+              телекоммуникации, энергетика и ТЭК. ЕвроМобайл создаёт решения для беспроводной связи на базе как
+              современных технологий: NB-IoT, LoRa, RFID, NFC, так и уже хорошо всем известных: LTE, 3G, Wi-Fi,
+              ГЛОНАСС.</p>
+            <a class="box__link" href="http://iot.ru/" target="_blank">Перейти на сайт</a>
+            <div class="box__caption">Официальный провайдер платформы:</div>
+            <a href="http://iot.ru/" target="_blank">
+              <img src="img/iot-logo-dark.svg" alt="логотип компании">
+            </a>
+            <h4 class="box__title-h4">Медиаресурс iot.ru</h4>
+            <p>iot.ru – первый и главный российский медиаресурс, посвященный Интернету вещей, искусственному
+              интеллекту,
+              Big Data, Cloud computing, AR, VR и другим важнейшим высоким технологиям нашего времени. IoT.ru регулярно
+              публикует новости, авторские статьи, аналитические отчёты, видеоинтервью с представителями бизнеса и
+              государства, видеообзоры самых важных профильных мероприятий.</p>
+            <a class="box__link" href="https://www.euromobile.ru/" target="_blank">Перейти на сайт</a>
+          </div>
+        </div>
+        <div class="box__item box__item--pic-one"></div>
+      </div>
+    </section>
+    <section id="advantages" class="advantages">
+      <div class="advantages__wrapper">
+        <h2 class="title advantages__title">Зачем участвовать?</h2>
+        <p class="advantages__caption">Лучшие проекты смогут сотрудничать с компанией «ЕвроМобайл» по одному из
+          следующих
+          сценариев</p>
+        <div class="advantages__box">
+          <div class="advantages__box-item">
+            <div class="advantages__inner">
+              <span class="advantages__count">01</span>
+              <h4 class="advantages__name advantages__name--icon-one">Пилотная площадка</h4>
+              <p class="advantages__text">Компания «ЕвроМобайл» готова предоставить пилотную площадку для
+                разворачивания
+                вашей технологии (компоненты для тестирования, площадку для испытаний)</p>
+            </div>
+          </div>
+          <div class="advantages__box-item">
+            <div class="advantages__inner">
+              <span class="advantages__count">02</span>
+              <h4 class="advantages__name advantages__name--icon-two">Включение в портфель инновационных решений</h4>
+              <p class="advantages__text">Ваши технологии или решения могут быть представлены партнёрам компании
+                «ЕвроМобайл» в России</p>
+            </div>
+          </div>
+          <div class="advantages__box-item">
+            <div class="advantages__inner">
+              <span class="advantages__count">03</span>
+              <h4 class="advantages__name advantages__name--icon-three">Инвестиции в проект</h4>
+              <p class="advantages__text">Самые перспективные и готовые к реализации проекты могут рассчитывать на
+                взаимовыгодное инвестиционное предложение со стороны компании «ЕвроМобайл»</p>
+            </div>
+          </div>
+          <div class="advantages__box-item">
+            <div class="advantages__inner">
+              <span class="advantages__count">04</span>
+              <h4 class="advantages__name advantages__name--icon-four">Маркетинговая поддержка</h4>
+              <p class="advantages__text">Компания «ЕвроМобайл» ведёт активную маркетинговую политику, авторам наиболее
+                перспективных проектов будет предложено совместное участие в маркетинговых мероприятиях</p>
+            </div>
+          </div>
+        </div>
+        <a class="logo logo-euro" href="https://www.euromobile.ru/" target="_blank">
+          <img src="img/euromob-logo-dark.svg" alt="">
+        </a>
+      </div>
+    </section>
+    <section id="conditions" class="conditions">
+      <div class="box">
+        <div class="box__item box__item--pic-two">
+          <a class="logo logo--iot" href="http://iot.ru/" target="_blank">
+            <img src="img/iot-logo.svg" alt="логотип провайдера">
+          </a>
+          <a class="logo logo--euro" href="https://www.euromobile.ru/" target="_blank">
+            <img src="img/euromob-logo.svg" alt="логотип компании">
+          </a>
+        </div>
+        <div class="box__item box__item--info">
+          <div class="box__info">
+            <h2 class="title box__title">Условия участия</h2>
+            <p>Компания «ЕвроМобайл» совместно с медиареурсом iot.ru приглашает к сотрудничеству практикующих ученых и
+              изобретателей, заинтересованных в воплощении своих разработок.</p>
+            <p>Предпочтение отдаётся дорожной, ЖКХ и энергетической отраслям, а также программному обеспечению, которое
+              разрабатывается для задач телематики, систем видеонаблюдения, интернета вещей.</p>
+            <p>Организаторы конкурса будут рассматривать и те проекты, имеющие потенциал для развития любой другой
+              отрасли, но по каким-либо причинам не получившие дальнейшего развития.</p>
+            <h4 class="box__title-h4">Ваша заявка* будет принята к рассмотрению, если она соответствует следующим
+              требованиям:</h4>
+            <ul class="list-numb">
+              <li class="list-numb__item">
+                <span class="list-numb__count">01</span>
+                <span class="list-numb__text">Изобретение до этого нигде не было реализовано и не получало
+                  коммерческого воплощения</span>
+              </li>
+              <li class="list-numb__item">
+                <span class="list-numb__count">02</span>
+                <span class="list-numb__text">Заявка на участие в конкурсе подана непосредственно автором или группой
+                  авторов изобретения</span>
+              </li>
+              <li class="list-numb__item">
+                <span class="list-numb__count">03</span>
+                <span class="list-numb__text">Заявка подана вовремя в указанные сроки: с 12 февраля по 14 апреля</span>
+              </li>
+            </ul>
+            <button class="button button-order popup-btn">Отправить заявку</button>
+          </div>
+          <p class="box-warning">* Компания «ЕвроМобайл и iot.ru гарантируют конфиденциальность полученной информации и
+            сохранность личных
+            данных</p>
+          <div class="copyright">Сделано в <a href="https://its.agency/" target="_blank">its.agency</a></div>
+        </div>
+      </div>
+    </section>
+    <div class="popup-overlay" id="form">
+      <section class="popup popup-order">
+        <button class="button popup-close" aria-label="Закрыть окно"></button>
+        <div class="popup-order__wrapper" id="form-body">
+          <h3 class="popup-order__title title">Заявка</h3>
+          <form class="popup-order__form" action="<?= $APPLICATION->GetCurPage(); ?>" method="post">
+            <ul class="popup-order__list">
+              <li class="popup-order__item">
+                <label class="visually-hidden" for="name">ФИО</label>
+                <input class="popup-order__input" id="name" type="text" name="FIO" placeholder="ФИО*" required="">
+              </li>
+              <li class="popup-order__item">
+                <label class="visually-hidden" for="phone">Контактный телефон*</label>
+                <input class="popup-order__input" id="phone" name="PHONE" type="tel" placeholder="Контактный телефон*"
+                  required="">
+              </li>
+              <li class="popup-order__item">
+                <label class="visually-hidden" for="email">E-mail</label>
+                <input class="popup-order__input" id="email" name="EMAIL" type="email" placeholder="E-mail*" required="">
+              </li>
+              <li class="popup-order__item">
+                <label class="visually-hidden" for="message">Комментарий</label>
+                <textarea class="popup-order__textarea" name="COMMENT" id="message" cols="30" rows="10" placeholder="Комментарий"></textarea>
+              </li>
+              <li class="popup-order__item">
+                <label for="myfile" class="popup-order__chous">Прикрепить файл (jpg, pdf, excel, word, zip, rar)</label>
+                <input type="file" class="popup-order__input popup-order__input--file" id="myfile" name="myfile"
+                  multiple="multiple">
+              </li>
+            </ul>
+            <div class="popup-order__bottom">
+              <p class="policy-text">Отправляя заявку, я подтверждаю,что ознакомлен и согласен с <a href="#">Политикой
+                  конфиденциальности</a> данного сайта</p>
+              <button class="button button-order popup-order__button" type="submit">Отправить заявку</button>
+            </div>
+          </form>
+        </div>
+        <div class="popup-order__wrapper" hidden id="form-success">
+            <p>Спасибо, ваша заявка на участие в конкурсе отправлена.</p>
+        </div>
+      </section>
+    </div>
+  </div>
+  <script src="js/vendor.min.js"></script>
+  <script src="js/main.min.js"></script>
+  <script src="js/form.js"></script>
+  <script>
+    svg4everybody();
+  </script>
+</body>
+
+</html>
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/epilog_after.php");
+?>
