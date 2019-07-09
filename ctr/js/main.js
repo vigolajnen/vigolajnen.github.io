1d6dcfaa13ca208b97cf26d7dcf2a3d4c8605251
vigolajnen.anna <vigolajnen@gmail.com>
2019-07-09 14:38:07 +0300

    hhh40

--- ctr/js/main.js
+++ ctr/js/main.js
@@ -65,6 +65,34 @@
     });
   });
 })();
+(function() {
+  var blocks = document.querySelectorAll(".block-w");
+  blocks.forEach(function(block) {
+    block.addEventListener("mouseover", function(evt) {
+      evt.preventDefault();
+
+      block.classList.add("block-w--br");
+      block.querySelector(".block-control").style.opacity = "1";
+      if (block.querySelector(".block-name"))
+        block.querySelector(".block-name").style.opacity = "1";
+
+      if (block.querySelector(".block-add"))
+        block.querySelector(".block-add").style.opacity = "1";
+    });
+
+    block.addEventListener("mouseout", function(evt) {
+      evt.preventDefault();
+
+      block.classList.remove("block-w--br");
+      block.querySelector(".block-control").style.opacity = "0";
+      if (block.querySelector(".block-name"))
+        block.querySelector(".block-name").style.opacity = "0";
+      if (block.querySelector(".block-add")) {
+        block.querySelector(".block-add").style.opacity = "0";
+      }
+    });
+  });
+})();
 
 
 (function() {
