108c24d6bc5e0ddff3e5b2774549dd98dea28a0e
vigolajnen.anna <vigolajnen@gmail.com>
2019-11-27 17:47:18 +0300

    отели

--- tsphere/js/main.js
+++ tsphere/js/main.js
@@ -159,6 +159,50 @@ popupOpenBtns.forEach(function(btn) {
     });
   }
 
+
+  var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    slidesPerColumn: 2,
+    direction: 'vertical',
+    loop: true,
+    freeMode: true,
+    loopedSlides: 4, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    mousewheel: true,
+    breakpoints: {
+      900: {
+        slidesPerView: 10,
+        spaceBetween: 20,
+        slidesPerColumn: 2,
+        direction: 'horizontal',
+      },
+      1024: {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        slidesPerColumn: 2,
+        direction: 'vertical',
+      },
+    }
+  });
+  var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    loop: true,
+    loopedSlides: 2, //looped slides should be the same
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs,
+    },
+  });
+
+
 })(); /* IIFE end */
 (function() {
   // var jsTriggers = document.querySelectorAll(".js-tab-trigger");
