1f830cf9a0c942653c52eabca5febadaa11dc2f2
vigolajnen.anna <vigolajnen@gmail.com>
2020-01-30 17:18:26 +0300

    demo

---
+++ demo/js/main.js
@@ -0,0 +1,15 @@
+
+"use strict";
+
+// Импортируем другие js-файлы
+$(document).ready(function () {
+  $("button").click(function () {
+    var elementClick = $(this).attr("data-target");
+    var destination = $(elementClick).offset().top;
+    $('html').animate({ scrollTop: destination }, 1100);
+    return false;
+  });
+});
+
+
+ScrollReveal().reveal('.headline');
