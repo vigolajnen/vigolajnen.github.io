db8ef4ee9aa292f63a9f711c5b21d1edae84c2dc
Anya Vigolajnen <avi26.job@yandex.ru>
2020-04-17 00:39:03 +0300

    ccc3

--- ccc/js/script.js
+++ ccc/js/script.js
@@ -13,3 +13,39 @@ headerToggle.addEventListener('click', function () {
     pageHeader.classList.remove('page-header--opened');
   }
 });
+
+
+
+(function() {
+  var nav = document.querySelector(".navigation");
+  var navBtn = nav.querySelector(".navigation__toggle");
+  var navInner = document.querySelector(".navigation__inner");
+
+  navBtn.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    console.log(navBtn);
+    if (navBtn.classList.contains("navigation__toggle--opened")) {
+      navBtn.classList.remove("navigation__toggle--opened");
+      navBtn.classList.add("navigation__toggle--closed");
+    } else {
+      navBtn.classList.add("navigation__toggle--opened");
+      navBtn.classList.remove("navigation__toggle--closed");
+    }
+
+    if (navInner.classList.contains("navigation__inner--active")) {
+      navInner.classList.remove("navigation__inner--active");
+      navInner.classList.add("navigation__inner--closed");
+    } else {
+      navInner.classList.add("navigation__inner--active");
+      navInner.classList.remove("navigation__inner--closed");
+    }
+
+    document.querySelector("body").classList.toggle("overlay");
+  });
+
+})();
+
+
+window.onresize = function () {
+  
+}
