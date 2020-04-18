18ae8c4dff94dfcfd72c4b7d43af1a6a6601b09b
Anya Vigolajnen <avi26.job@yandex.ru>
2020-04-19 01:50:54 +0300

    cruise map

--- cruise/js/script.js
+++ cruise/js/script.js
@@ -41,3 +41,17 @@
   }
 
 })();
+function initMap() {
+  var myLatLng = { lat: 59.938635, lng: 30.323118 };
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'г. Санкт Петербург, ул. Большая Конюшенная, 19/8'
+  });
+}
