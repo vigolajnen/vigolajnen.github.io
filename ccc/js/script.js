b35359d48e56ec395d5e80fd7e45b683637bc8ce
Anya Vigolajnen <avi26.job@yandex.ru>
2020-04-21 18:58:06 +0300

    55

--- ccc/js/script.js
+++ ccc/js/script.js
@@ -1,51 +1,45 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+(function () {
+  var nav = document.querySelector('.navigation');
+  var navBtn = nav.querySelector('.navigation__toggle');
+  var navInner = document.querySelector('.navigation__inner');
+  var logo = document.querySelector('.logo');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
-
-
-
-(function() {
-  var nav = document.querySelector(".navigation");
-  var navBtn = nav.querySelector(".navigation__toggle");
-  var navInner = document.querySelector(".navigation__inner");
-
-  navBtn.addEventListener("click", function(evt) {
+  navBtn.addEventListener('click', function (evt) {
     evt.preventDefault();
-    console.log(navBtn);
-    if (navBtn.classList.contains("navigation__toggle--opened")) {
-      navBtn.classList.remove("navigation__toggle--opened");
-      navBtn.classList.add("navigation__toggle--closed");
+    if (navBtn.classList.contains('navigation__toggle--opened')) {
+      navBtn.classList.remove('navigation__toggle--opened');
+      navBtn.classList.add('navigation__toggle--closed');
+      logo.classList.remove('logo--mob');
     } else {
-      navBtn.classList.add("navigation__toggle--opened");
-      navBtn.classList.remove("navigation__toggle--closed");
+      navBtn.classList.add('navigation__toggle--opened');
+      navBtn.classList.remove('navigation__toggle--closed');
+      logo.classList.add('logo--mob');
     }
 
-    if (navInner.classList.contains("navigation__inner--active")) {
-      navInner.classList.remove("navigation__inner--active");
-      navInner.classList.add("navigation__inner--closed");
+    if (navInner.classList.contains('navigation__inner--active')) {
+      navInner.classList.remove('navigation__inner--active');
+      navInner.classList.add('navigation__inner--closed');
     } else {
-      navInner.classList.add("navigation__inner--active");
-      navInner.classList.remove("navigation__inner--closed");
+      navInner.classList.add('navigation__inner--active');
+      navInner.classList.remove('navigation__inner--closed');
     }
-
-    document.querySelector("body").classList.toggle("overlay");
+    document.querySelector('body').classList.toggle('overlay');
   });
 
+  window.onresize = function () {
+    if (window.screen.width > 768) {
+      document.querySelector('body').classList.remove('overlay');
+      if (navInner.classList.contains('navigation__inner--active')) {
+        navInner.classList.remove('navigation__inner--active');
+      }
+      if (navInner.classList.contains('navigation__inner--closed')) {
+        navInner.classList.remove('navigation__inner--closed');
+      }
+      if (navBtn.classList.contains('navigation__toggle--opened')) {
+        navBtn.classList.remove('navigation__toggle--opened');
+      }
+    }
+  };
 })();
-
-
-window.onresize = function () {
-  
-}
--- ccc/js/vendor.js
+++ ccc/js/vendor.js
@@ -1,3 +1,4 @@
+'use strict';
 !function(root, factory) {
     "function" == typeof define && define.amd ? // AMD. Register as an anonymous module unless amdModuleId is set
     define([], function() {
@@ -104,3 +105,4 @@
     }
     return svg4everybody;
 });
+svg4everybody();
