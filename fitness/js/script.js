a15709352e9cf1e86d0789618d1f8ea8e8948e38
Anya Vigolajnen <avi26.job@yandex.ru>
2020-05-13 12:45:22 +0300

    hh

--- fitness/js/script.js
+++ fitness/js/script.js
@@ -1,39 +1,39 @@
 'use strict';
 
-if ('NodeList' in window && !NodeList.prototype.forEach) {
-
-  NodeList.prototype.forEach = function (callback, thisArg) {
-    thisArg = thisArg || window;
-    for (var i = 0; i < this.length; i++) {
-      callback.call(thisArg, this[i], i, this);
-    }
-  };
-}
-
-var itemTabs = document.querySelectorAll('.tabs__item');
-
-if (itemTabs) {
-  itemTabs.forEach(function (item) {
-    item.addEventListener('click', function () {
-      var activeItem = document.querySelector('.tabs__item.tabs__item--active');
-      activeItem.classList.remove('tabs__item--active');
-      item.classList.add('tabs__item--active');
-    });
-  });
-}
-
-var phone = document.querySelector('#phone');
-if (phone) {
-  phone.addEventListener('input', function () {
-    if (phone.value.length < 16) {
-      phone.setCustomValidity('Введите номер телефона полностью');
-    } else {
-      phone.setCustomValidity('');
-    }
-  });
-}
-
 (function () {
+
+  if ('NodeList' in window && !NodeList.prototype.forEach) {
+
+    NodeList.prototype.forEach = function (callback, thisArg) {
+      thisArg = thisArg || window;
+      for (var i = 0; i < this.length; i++) {
+        callback.call(thisArg, this[i], i, this);
+      }
+    };
+  }
+
+  var itemTabs = document.querySelectorAll('.tabs__item');
+
+  if (itemTabs) {
+    itemTabs.forEach(function (item) {
+      item.addEventListener('click', function () {
+        var activeItem = document.querySelector('.tabs__item.tabs__item--active');
+        activeItem.classList.remove('tabs__item--active');
+        item.classList.add('tabs__item--active');
+      });
+    });
+  }
+
+  var phone = document.querySelector('#phone');
+  if (phone) {
+    phone.addEventListener('input', function () {
+      if (phone.value.length < 16) {
+        phone.setCustomValidity('Введите номер телефона полностью');
+      } else {
+        phone.setCustomValidity('');
+      }
+    });
+  }
 
   var hoursDB = ['08:00', '12:00', '18:00', '20:00'];
 
