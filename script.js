9ded42150e37bbca10639ff815bf3b5d216f6d92
Anya Vigolajnen <vigolajnen@gmail.com>
2021-12-14 15:47:56 +0300

    dark theme

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-// Выбираем кнопку
 const btn = document.querySelector(".btn-toggle");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-// Отслеживаем щелчок по кнопке
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-theme");
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
+}
+
 btn.addEventListener("click", function () {
-  // Если ОС настроена в тёмном режиме…
   if (prefersDarkScheme.matches) {
-    // …тогда применяем класс .light-theme для переопределения этих стилей
     document.body.classList.toggle("light-theme");
-    // В противном случае…
+    var theme = document.body.classList.contains("light-theme")
+      ? "light"
+      : "dark";
   } else {
-    // …применяем класс .dark-theme для переопределения светлого стиля по умолчанию
     document.body.classList.toggle("dark-theme");
+    var theme = document.body.classList.contains("dark-theme")
+      ? "dark"
+      : "light";
   }
+  localStorage.setItem("theme", theme);
 });
